6754521a9cfb4b0d41edc41d3baf34d5fd8b65e5
Lucas Minter <lsminteris@gmail.com>
2023-05-30 16:54:10 -0700

    newly styled npc generator page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -46,7 +46,7 @@ const Profile = () => {
       <div>
         {!user && 
           <Auth
-            redirectTo={'http://localhost:3000/home'}
+            redirectTo={'http://localhost:3000/'}
             appearance={{ theme: ThemeSupa }}
             supabaseClient={supabase}
             providers={['google']}
